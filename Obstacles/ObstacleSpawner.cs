2537e95a266a8234f58a7458fb1641bc9be427b2
George Brackpool <71015353+GeorgeBrackpool@users.noreply.github.com>
2022-06-06 13:30:40 +0100

    Added looping to wave spawns. Need to add a way to randomise through the wave list.

--- Obstacles/ObstacleSpawner.cs
+++ Obstacles/ObstacleSpawner.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] ObstacleSpawnConfig currentWave;
+    [SerializeField] List<ObstacleSpawnConfig> waveConfiguration;
+    [SerializeField] float timeBetweenObstacleWaves = 0f;
+    [SerializeField] bool isLoopingWaves;
+    ObstacleSpawnConfig currentWave;
     // Start is called before the first frame update
     void Start()
     {
-        SpawnObstacles();
+       StartCoroutine(SpawnObstacles());
     }
 
-    void SpawnObstacles()
+    public ObstacleSpawnConfig GetCurrentWave()
     {
-        for (int i = 0; i < currentWave.GetObstacleCount(); i++){
-        Instantiate(currentWave.GetObstaclePrefab(i),
-        transform.position,
-        Quaternion.identity, transform);
+        return currentWave;
+    }
+    IEnumerator SpawnObstacles()
+    {
+        do{
+        foreach(ObstacleSpawnConfig wave in waveConfiguration)
+            {
+            currentWave = wave;
+            //currentWave = Random.Range(0,waveConfiguration.Count);
+            for (int i = 0; i < currentWave.GetObstacleCount(); i++)
+            {
+                Instantiate(currentWave.GetObstaclePrefab(i),
+                transform.position,
+                Quaternion.identity, transform);
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+            }
+            yield return new WaitForSeconds(timeBetweenObstacleWaves);//TODO: randomise waves.
+        
+            }
         }
+        while(isLoopingWaves == true); 
     }
 }
